27421e3c0b53510caf91092df7864513b37327ac
Nick6464 <49857434+Nick6464@users.noreply.github.com>
2021-04-24 16:33:20 +1200

    Improved the Column Splitting

--- main.js
+++ main.js
@@ -1,64 +1,55 @@
-const PublicGoogleSheetsParser = require("public-google-sheets-parser");
+/*
+Author: Nick Walton
+Date Created: 24/04/2021
 
-const spreadsheetId = "17zo5TRXxyMkAJ4WQBMmXey6jMMsHcnwA-ULVeM5YXsc";
+The purpose of this program is to decrease load times for my website by preloading 
+Google sheet data by saving it as a JSON file periodically
+*/
 
-//Create Parser linked to the Public Sheet
-const parser = new PublicGoogleSheetsParser(spreadsheetId);
+/**
+ * Where the Sheet is created and
+ */
+function main() {
+  const PublicGoogleSheetsParser = require("public-google-sheets-parser");
 
-//Testing for output
-parser.parse().then((items) => {
-  proccessSheet(items);
-});
+  //Link from sheet URL
+  const spreadsheetId = "17zo5TRXxyMkAJ4WQBMmXey6jMMsHcnwA-ULVeM5YXsc";
 
-function proccessSheet(sheetData){
-    let dataByColunms = columnSpliter(items);
+  //Create Parser linked to the Public Sheet
+  const parser = new PublicGoogleSheetsParser(spreadsheetId);
+  //Testing for output
+  parser.parse().then((items) => {
+    processSheet(items);
+  });
 }
 
-//Takes the spread sheet and spilts it into lists based on the column
+/**
+ * Takes the Sheet data and processses to exported as a JSON
+ * @param {List of Dictionaries} sheetData
+ */
+function processSheet(sheetData) {
+  let dataByColunms = columnSpliter(sheetData);
+}
+
+/**
+ * Converts the Sheet from being sorted by Rows to Colunms
+ * Adds each cell to the list of other cells from the same column
+ * @param {List of Dictionaries} sheet
+ * @returns Dictionary of Lists
+ */
 function columnSpliter(sheet) {
-  let columns = {
-    world: [],
-    time: [],
-    village: [],
-    biome: [],
-    diamonds: [],
-    causeOfDeath: [],
-    deathLocation: [],
-    deadPlayer: [],
-    numberOfPlayers: [],
-    playerNames: [],
-    gearTier: [],
-    timeToNether: [],
-    netherite: [],
-    timeToEnd: [],
-    pinkSheep: [],
-    babyJockey: [],
-    nightsSpentAwake: [],
-    nightsSlept: [],
-    petsTamed: [],
-    notes: [],
-  };
+  let columns = {};
   sheet.forEach((element) => {
-    columns.world.push(element['World']);
-    columns.time.push(element['Time']);
-    columns.village.push(element['Village']);
-    columns.biome.push(element['Biome']);
-    columns.diamonds.push(element['Diamonds']);
-    columns.causeOfDeath.push(element['Cause of Death']);
-    columns.deathLocation.push(element['Location of Death']);
-    columns.deadPlayer.push(element['Player Who Died']);
-    columns.numberOfPlayers.push(element['# Of Player']);
-    columns.playerNames.push(element['List of Players']);
-    columns.gearTier.push(element['Final Gear']);
-    columns.timeToNether.push(element['Time to Nether']);
-    columns.netherite.push(element['Netherite']);
-    columns.timeToEnd.push(element['Time to End']);
-    columns.pinkSheep.push(element['Pink Sheep']);
-    columns.babyJockey.push(element['Baby Jockey']);
-    columns.nightsSpentAwake.push(element['Nights Spent Awake']);
-    columns.nightsSlept.push(element['Times Slept']);
-    columns.petsTamed.push(element['Pets Tamed']);
-    columns.notes.push(element['World Notes']);
+    Object.keys(element).forEach((item) => {
+      //columns.item.push(element[item]);
+      if (!(item in columns)) {
+        columns[item] = [element[item]];
+      } else {
+          columns[item].push(element[item]);
+      }
+    });
   });
   return columns;
 }
+
+main();
